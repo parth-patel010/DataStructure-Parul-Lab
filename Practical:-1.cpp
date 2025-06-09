b9d48127d57b891917eeaa865136841b17d80dd2
Parth_Patel__. <parthp12321@gmail.com>
2025-06-09 22:01:31 +0530

    pratical 1

--- Practical:-1.cpp
+++ Practical:-1.cpp
@@ -1,1 +1,33 @@
+//Practical:-1
+//Write a program to Implement Stack operations using arrays. 
+//top,push,pop,empty,size
 
+/*
+===Expected Output===
+Top element of stack: 3
+Size of Stack after removing top: 3
+Top element of stack: 2
+Size of Stack after removing top: 2
+Top element of stack: 1
+Size of Stack after removing top: 1
+*/
+
+#include<iostream>
+#include<stack>
+
+void printStackElement(std::stack <int> stack){
+    while(!stack.empty()){
+        std::cout << "Top element of stack: " << stack.top() << std::endl;
+        std::cout << "Size of Stack after removing top: "<<stack.size() << std::endl;
+        stack.pop();
+    }
+}
+
+int main(){
+    std::stack<int> stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    printStackElement(stack);
+    return 0;
+}
